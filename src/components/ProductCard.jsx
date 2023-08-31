e9d0b3655390c4f14e1f8e238ca1fe60e2b9cb85
Marcelo Bettini <bettinimarcelo@gmail.com>
2023-08-31 16:46:22 -0300

    expand description

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,32 @@
+import { MdExpandLess, MdExpandMore } from "react-icons/md";
 import "./ProductCard.css";
+import { useState } from "react";
 function ProductCard({ p }) {
+  const [isFullDescription, setIsFullDescription] = useState(false);
+  const handleExpand = () => {
+    setIsFullDescription(!isFullDescription);
+  };
   return (
     <article>
       <h3>{p.title}</h3>
-      <img src={p.image} alt={p.title} />
-      <p>{p.category}</p>
-      <p>{p.price}</p>
-      <p>{p.description}</p>
+      <div style={{ display: "flex" }}>
+        <img src={p.image} alt={p.title} style={{ width: "50%" }} />
+        <div>
+          <p>{p.category}</p>
+          <p>{p.price}</p>
+          <button>add to cart</button>
+        </div>
+      </div>
+      {isFullDescription ? (
+        <span>
+          {p.description} <MdExpandLess size={27} onClick={handleExpand} />
+        </span>
+      ) : (
+        <span>
+          {p.description.slice(0, 40)}...{" "}
+          <MdExpandMore size={27} onClick={handleExpand} />
+        </span>
+      )}
     </article>
   );
 }
